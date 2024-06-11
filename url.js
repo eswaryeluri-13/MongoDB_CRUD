f9da94010937bb887c928d3affa466fde22cee24
Eswar Yeluri <eswaryeluri13@gmail.com>
2024-06-11 14:16:09 +0530

    intial commit

--- url.js
+++ url.js
@@ -1,1 +1,2 @@
-module.exports = `mongodb://localhost:27017`
+/*module.exports = `mongodb://localhost:27017`*/
+module.exports='mongodb+srv://eswaryeluri13:eswaryeluri13@cluster3.lgatztf.mongodb.net/'
